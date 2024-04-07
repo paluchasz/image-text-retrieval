57d7cf1b4c9efca5c9abdc48ccebbc0b827aa8ef
Szymon Palucha <paluchasz@googlemail.com>
2024-04-07 21:12:17 +0100

    feat: add titles and dividers

--- image_text_retrieval/ui/app_ui.py
+++ image_text_retrieval/ui/app_ui.py
@@ -5,6 +5,11 @@ import httpx
 import pandas as pd
 import streamlit as st
 
+st.title("Image Text Retrieval Playground")
+
+st.divider()
+st.header("Search for an image with a description below")
+
 st.text_input("Write an image description", key="image_description")
 
 
@@ -13,6 +18,8 @@ if image_description := st.session_state.image_description:
     response = httpx.post(url="http://0.0.0.0:8000/retrieve_image_from_text", json={"text": image_description})
     st.image(io.BytesIO(response.content))
 
+st.divider()
+st.header("Search for captions with an image")
 
 st.file_uploader("Upload an image to search for captions", key="uploaded_image")
 uploaded_image = st.session_state.uploaded_image
